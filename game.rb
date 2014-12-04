e9b23e043016bb0b1d6c256e93382a9035c19bd5
shinokaro <shinokaro@hotmail.co.jp>
2014-12-04 11:18:50 +0900

    game
    メンバー変数へのアクセスをインスタンス変数経由にした。
    cleanupの実行順序をdraw内部とそろえる。
    update内のSprite.checkをdraw内部とそろえる。
    どちらもゲーム進行には影響しない。

--- game.rb
+++ game.rb
@@ -7,7 +7,7 @@ class Game
     @screen = RenderTarget.new(Window.width/2, Window.height/2)
     @stage  = Stage.new(self, stage_data)
     @player = Player.new(Assets[:player])
-    @player.target    = self.screen
+    @player.target    = @screen
     @player.collision = [5, 3, 10, 12]
     setup
   end
@@ -33,17 +33,17 @@ class Game
   end
 
   def update
-    stage.update
-    if player.alive?
-      player.update_input
-      shots.push(*player.fire) if Input.key_push?(K_Z)
-      player.update
-      Sprite.update(shots)
-      Sprite.update(enemies)
-      Sprite.update(bullets)
-      Sprite.check(enemies, player)
-      Sprite.check(bullets, player)
-      Sprite.check(shots, enemies)
+    @stage.update
+    if @player.alive?
+      @player.update_input
+      @shots.push(*@player.fire) if Input.key_push?(K_Z)
+      @player.update
+      Sprite.update(@shots)
+      Sprite.update(@enemies)
+      Sprite.update(@bullets)
+      Sprite.check(@bullets, @player)
+      Sprite.check(@enemies, @player)
+      Sprite.check(@shots,   @enemies)
       true
     else
       false
@@ -51,26 +51,26 @@ class Game
   end
 
   def cleanup
-    shots.each do |shot|
-      shot.vanish unless self.stage === shot
+    @bullets.each do |bullet|
+      bullet.vanish unless @stage === bullet
     end
-    enemies.each do |enemy|
-      enemy.vanish unless self.stage === enemy
+    @enemies.each do |enemy|
+      enemy.vanish  unless @stage === enemy
     end
-    bullets.each do |bullet|
-      bullet.vanish unless self.stage === bullet
+    @shots.each   do |shot|
+      shot.vanish   unless @stage === shot
     end
-    Sprite.clean(shots)
-    Sprite.clean(enemies)
-    Sprite.clean(bullets)
+    Sprite.clean(@bullets)
+    Sprite.clean(@enemies)
+    Sprite.clean(@shots)
   end
 
   def draw
-    Sprite.draw(bullets)
-    Sprite.draw(enemies)
-    Sprite.draw(shots)
-    player.draw
-    Window.draw_scale(Window.width/4, Window.height/4, screen, 2, 2)
+    Sprite.draw(@bullets)
+    Sprite.draw(@enemies)
+    Sprite.draw(@shots)
+    @player.draw
+    Window.draw_scale(Window.width/4, Window.height/4, @screen, 2, 2)
   end
 
 end
